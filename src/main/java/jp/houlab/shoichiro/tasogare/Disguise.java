e102612465caf30c822aa8fcf713e1f004a5fe7d
BuildTools <unconfigured@null.spigotmc.org>
2024-05-11 14:30:08 +0900

    save

--- src/main/java/jp/houlab/shoichiro/tasogare/Disguise.java
+++ src/main/java/jp/houlab/shoichiro/tasogare/Disguise.java
@@ -39,6 +39,8 @@ public class Disguise implements Listener {
             if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK || event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK) {
                     if (event.getMaterial().equals(Material.GLOWSTONE_DUST)) {
 
+                        new RemainScheduler(player).runTaskTimer(Tasogare.getPlugin(),0,20);
+
                         if (team.getName().equals(team1.getName())) {
                             List<String> enemy2 = new ArrayList<>(team2.getEntries());
                             int i = new Random().nextInt(enemy2.size());
@@ -155,6 +157,7 @@ public class Disguise implements Listener {
                             Set<ProfileProperty> propertyEnemy = profileEnemy.getProperties();
                             PlayerTextures skinEnemy = profileEnemy.getTextures();
                             URL urlEnemy = skinEnemy.getSkin();
+
 
                             skinEnemy.setSkin(urlEnemy);
                             profile.setTextures(skinEnemy);
---
+++ src/main/java/jp/houlab/shoichiro/tasogare/RemainScheduler.java
@@ -0,0 +1,34 @@
+package jp.houlab.shoichiro.tasogare;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.title.Title;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.time.Duration;
+
+public class RemainScheduler extends BukkitRunnable {
+    int time;
+    Player player;
+
+    public RemainScheduler(Player player) {
+        this.player = player;
+        this.time = 20;
+    }
+
+    @Override
+    public void run() {
+        time--;
+
+        if (time < 0) {
+            player.clearTitle();
+            cancel();
+            return;
+        }
+
+        if (time > 0 && time > 0 && time > 0) {
+            Title title = Title.title(Component.text(""), Component.text("ULT残り" + time + "秒"), Title.Times.times(Duration.ofSeconds(0), Duration.ofSeconds(20), Duration.ofSeconds(0)));
+            player.showTitle(title);
+        }
+    }
+}
