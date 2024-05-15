26c2192705c80c7485beb505ce73bc9af18b0b3e
BuildTools <unconfigured@null.spigotmc.org>
2024-05-16 01:37:40 +0900

    save

--- src/main/java/jp/houlab/shoichiro/tasogare/CenterScheduler.java
+++ src/main/java/jp/houlab/shoichiro/tasogare/CenterScheduler.java
@@ -25,10 +25,9 @@ PlayerProfile playerProfile;
     }
     @Override
     public void run(){
-        player.getWorld().spawnParticle(Particle.SPIT, player.getX(), player.getY(), player.getZ(), 800, 0.3, 1, 0.3, 0);
-        player.getWorld().spawnParticle(Particle.TRIAL_SPAWNER_DETECTION,player.getX(), player.getY(), player.getZ(), 600, 0.3, 2, 0.3, 0);
-        player.getWorld().spawnParticle(Particle.WARPED_SPORE, player.getX(), player.getY(), player.getZ(), 500, 1, 1, 1, 0);
-       location.getWorld().playSound(location, Sound.ENTITY_GHAST_SHOOT, 1, 1);
+        player.getWorld().spawnParticle(Particle.DUST_PLUME, player.getX(), player.getY(), player.getZ(), 800, 0.3, 1.5, 0.3, 0);
+        player.getWorld().spawnParticle(Particle.TRIAL_SPAWNER_DETECTION,player.getX(), player.getY(), player.getZ(), 600, 0.3, 1, 0.3, 0);
+        location.getWorld().playSound(location, Sound.ENTITY_GHAST_SHOOT, 1, 1);
 
         player.setPlayerProfile(playerProfile);
 
